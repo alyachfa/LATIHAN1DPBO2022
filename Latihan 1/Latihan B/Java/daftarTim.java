d6183591f8b88686703180c58b492dbc64fde5c2
alyachfa <75361865+alyachfa@users.noreply.github.com>
2022-02-14 23:23:36 +0700

    Update daftarTim.java

--- Latihan 1/Latihan B/Java/daftarTim.java
+++ Latihan 1/Latihan B/Java/daftarTim.java
@@ -1,1 +1,42 @@
+class daftarTim{
+	
+	private String namaTim;
+	private String negaraAsal;
+	private String tahun;
+	private String pemain;
 
+	public daftarTim(){
+	}
+
+	public void setNamaTim(String namaTim){
+		this.namaTim = namaTim;
+	}
+
+	public String getNamaTim(){
+		return this.namaTim;
+	}
+
+	public void setNegaraAsal(String negaraAsal){
+		this.negaraAsal = negaraAsal;
+	}
+
+	public String getNegaraAsal(){
+		return this.negaraAsal;
+	}
+
+	public void setTahun(String tahun){
+		this.tahun = tahun;
+	}
+
+	public String getTahun(){
+		return this.tahun;
+	}
+
+	public void setPemain(String pemain){
+		this.pemain = pemain;
+	}
+
+	public String getPemain(){
+		return this.pemain;
+	}
+}
