8d649626ebb5b39b6785ffee07da2ae2f322f462
alyachfa <75361865+alyachfa@users.noreply.github.com>
2022-02-14 22:12:34 +0700

    Add files via upload

---
+++ Latihan 1/Latihan B/C++/main.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdlc++.h>
+#include "daftarTim.cpp"
+using namespace std;
+
+int main(){
+    string namaTim;
+    string negaraAsal;
+    string tahun;
+    string pemain;
+    string pelatih;  
+
+    int jmlTim = 0;
+    cout<<"Masukan Jumlah Tim : ";
+    cin>>jmlTim;
+
+    daftarTim arr[jmlTim];
+
+    cout <<"namaTim : ";
+    cin >> namaTim;
+
+    cout << "negaraAsal : ";
+    cin >> negaraAsal;
+
+    cout << "tahun : ";
+    cin >> tahun;
+
+    int jmlPemain;
+    cout<< "Masukan Jumlah Pemain : ";
+    cin>> jmlPemain;
+    
+}
