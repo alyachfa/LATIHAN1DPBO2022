ba375e2080f824c1ced0cdc57be7e03d7f4cf65e
alyachfa <75361865+alyachfa@users.noreply.github.com>
2022-02-14 22:07:59 +0700

    Update index.php

--- Latihan 1/Latihan A/index.php
+++ Latihan 1/Latihan A/index.php
@@ -4,6 +4,7 @@
     $mahasiswa1 = new DataMahasiswa();
     $mahasiswa1->setNim('1902345');
     $mahasiswa1->setNama('asep');
+    $mahasiswa1->setjenisKelamin('laki-laki');
     $mahasiswa1->setProdi('Ilmu Komputer');
     $mahasiswa1->setSemester('6');
 
@@ -11,11 +12,13 @@
 
     echo "Nim : ". $mahasiswa1->getNim() . "<br>";
     echo "Nama : ". $mahasiswa1->getNama(). "<br>";
+    echo "Jenis Kelamin : ". $mahasiswa1->getjenisKelamin(). "<br>";
     echo "Program Studi : ".$mahasiswa1->getProdi(). "<br>";
     echo "Semester : ".$mahasiswa1->getSemester(). "<br>"; 
 
     echo "Nim : ". $mahasiswa2->getNim() . "<br>";
     echo "Nama : ". $mahasiswa2->getNama(). "<br>";
+    echo "Jenis Kelamin : ". $mahasiswa2->getjenisKelamin(). "<br>";
     echo "Program Studi : ".$mahasiswa2->getProdi(). "<br>";
     echo "Semester : ".$mahasiswa2->getSemester(). "<br>"; 
 ?>
