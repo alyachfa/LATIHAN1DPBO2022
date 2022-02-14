4e84a3ee38d3e226d677fc942ab55c2967560f3f
alyachfa <75361865+alyachfa@users.noreply.github.com>
2022-02-14 23:17:54 +0700

    Update daftarTim.py

--- Latihan 1/Latihan B/Python/daftarTim.py
+++ Latihan 1/Latihan B/Python/daftarTim.py
@@ -1,1 +1,31 @@
+class daftarTim():
 
+	def __init__(self, namaTim, negaraAsal, tahun, pemain):
+		self.__namaTim = namaTim
+		self.__negaraAsal = negaraAsal
+		self.__tahun = tahun
+		self.__pemain = pemain
+
+	def setNama(self, namaTim):
+		self.__namaTim = namaTim
+
+	def getNamaTim(self):
+		return self.__namaTim
+
+	def setNegaraAsal(self, negaraAsal):
+		self.__negaraAsal = negaraAsal
+
+	def getNegaraAsal(self):
+		return self.__negaraAsal
+
+	def setTahun(self, tahun):
+		self.__tahun = tahun
+
+	def getTahun(self):
+		return self.__tahun
+
+	def setPemain(self, pemain):
+		self.__pemain = pemain
+
+	def getPemain(self):
+		return self.__pemain
