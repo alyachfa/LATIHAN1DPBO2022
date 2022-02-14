a9dd3484ecec4b33bc87e6f0971a94605a68a00b
alyachfa <75361865+alyachfa@users.noreply.github.com>
2022-02-14 23:20:42 +0700

    Update daftarTim.php

--- Latihan 1/Latihan B/PHP/daftarTim.php
+++ Latihan 1/Latihan B/PHP/daftarTim.php
@@ -1,1 +1,55 @@
+<?php
 
+class daftarTim{
+
+	private $namaTim;
+	private $negaraAsal;
+	private $tahun;
+	private $pemain;
+
+	public function __construct(){
+	}
+
+	public function setNamaTim($namaTim)
+	{
+		$this->namaTim = $namaTim;
+	}
+
+	public function getNamaTim()
+	{
+		return $this->namaTim;
+	}
+
+	public function setNegaraAsal($negaraAsal)
+	{
+		$this->negaraAsal = $negaraAsal;
+	}
+
+	public function getNegaraAsal()
+	{
+		return $this->negaraAsal;
+	}
+
+	public function setTahun($tahun)
+	{
+		$this->tahun = $tahun;
+	}
+
+	public function getTahun()
+	{
+		return $this->tahun;
+	}
+
+	public function setPemain($pemain)
+	{
+		$this->pemain = $pemain;
+	}
+
+	public function getPemain()
+	{
+		return $this->pemain;
+	}
+
+}
+
+?>
